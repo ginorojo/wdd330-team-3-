6cfcafe62a70238e705842124539264cab2ab95c
mignrod <mignrod@gmail.com>
2025-05-15 12:53:37 -0400

    fixing some lint errors. Solution week 1 and 2 added.

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,10 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./productDetails.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-
+//Creting a new instance of the ProductData class
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 const Product = new ProductDetails(productId, dataSource);
 // initialize product details
 Product.init();
