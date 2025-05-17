b2e3c10686ca145a64da064f775b06a930a88d6b
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2025-05-17 00:58:41 -0300

    Duplicate items in cart - Completed

--- src/js/cart.js
+++ src/js/cart.js
@@ -9,6 +9,7 @@ function renderCartContents() {
 // Adding a funtion to populate the cart with a template
 // This function will be used to create the HTML for each item in the cart
 function cartItemTemplate(item) {
+  const quantity = item.quantity || 1;
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
@@ -20,7 +21,7 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">qty: ${quantity}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
