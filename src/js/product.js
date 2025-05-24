5317300b56b07378b8184d8cdf1ff829bd16ba31
mignrod <mignrod@gmail.com>
2025-05-24 18:51:32 -0400

    Individual week 3

--- src/js/product.js
+++ src/js/product.js
@@ -3,10 +3,12 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
+loadHeaderFooter();
+
 //Creting a new instance of the ProductData class
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 const Product = new ProductDetails(productId, dataSource);
+
 // initialize product details
 Product.init();
-loadHeaderFooter();
