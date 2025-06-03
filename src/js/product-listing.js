49a7bae0c0f1b544706d5264bcfe77c62c936fb9
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2025-06-03 20:02:00 -0300

    Product List sort

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -12,4 +12,28 @@ const element = document.querySelector(".product-list");
 
 const productlist = new ProductList(category, dataSource, element);
 
+let originalList = [];
+
+async function sortedListnit() {
+  const list = await dataSource.getData(category);
+  originalList = list;
+  productlist.renderList(originalList);
+  document.querySelector(".title").textContent = category;
+}
+
+sortedListnit()
+
 productlist.init();
+
+document.getElementById("sort").addEventListener("change", (e) => {
+    let sortedList = [...originalList]; // cópia da lista original
+    const value = e.target.value;
+  
+    if (value === "name") {
+      sortedList.sort((a, b) => a.NameWithoutBrand.localeCompare(b.NameWithoutBrand));
+    } else if (value === "price") {
+      sortedList.sort((a, b) => a.FinalPrice - b.FinalPrice);
+    }
+  
+    productlist.renderList(sortedList);
+});
